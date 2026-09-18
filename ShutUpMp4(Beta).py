6f13a70014e1e854efcc848cbde22fcb8fe7952c
gubbygui <73776973+gubbygui@users.noreply.github.com>
2026-09-18 13:17:23 +0200

    Implement GUI for file selection
    Added a GUI file dialog to select video files.

--- ShutUpMp4(Beta).py
+++ ShutUpMp4(Beta).py
@@ -1,11 +1,28 @@
 import os
 from moviepy import VideoFileClip
+from tkinter import *
+from tkinter import filedialog
 
+def openFile():
+    path = filedialog.askopenfilename()
+    return path
+
+def OpenWindow():
+    Window = Tk()
+    button = Button(text="Click me to open the Explorer!", command=openFile)
+    button.pack()
+    Window.mainloop()
+
+print("Welcome to Shut Up Mp4! Enter GUI to open the file browser! ")
 video_path = input("Enter the path to the video file: ").strip().strip('"')
 output_name = input("Enter the name of the output video file (without extension): ").strip()
 
+if video_path == "GUI":
+    video_path = openFile()
+
+
 if not video_path:
-    raise ValueError("you didnt even enter anything")
+    raise ValueError("you didnt even enter anything or closed the window")
 
 if not os.path.exists(video_path):
     raise FileNotFoundError(f"Video file not found: {video_path}")
